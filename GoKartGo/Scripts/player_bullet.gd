02e3c0184f9ddab160e77abd33e65ece6bfd0377
zstowers <zstowers@aggienetwork.com>
2017-12-12 08:51:30 -0600

    update

--- GoKartGo/Scripts/player_bullet.gd
+++ GoKartGo/Scripts/player_bullet.gd
@@ -14,7 +14,13 @@ func start_at(dir, pos):
 func _fixed_process(delta):
 	set_pos(get_pos() + vel * delta)
 	
-
+func _on_player_bullet_area_enter( area ):
+	if area.get_groups().has("flying_enemy"):
+		print("Entered flying enemy")
+	if area.get_groups().has("turret"):
+		print("Entered turret")
+		#queue_free()
+		#area.damage(global.bullet_damage)
 
 func _on_visible_exit_screen():
 	queue_free()
---
+++ GoKartGo/Scripts/turret_ball.gd
@@ -0,0 +1,51 @@
+extends KinematicBody2D
+
+export (bool) var explode_on_impact = true
+export (int) var damage = 25
+
+#gravity
+var _gravity = 0
+
+# Movement
+var _movement = Vector2()
+
+func shoot(directional_force, gravity):
+	_movement = directional_force
+	_gravity = gravity
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	#Simulate gravity
+	_movement.y <= delta * _gravity
+	
+	#check if we have collided
+	if(is_colliding()):
+		if(explode_on_impact):
+			explode()
+			set_fixed_process(false)
+			return
+		
+		#Get node that we are colliding with
+		var entity = get_collider()
+		
+		#apply physics
+		var normal = get_collision_normal()
+	#move
+	move(_movement)
+	
+	#if(get_pos().x > OS.get_screen_size().x):
+		#queue_free()
+	#if(get_pos().y > OS.get_screen_size().y):
+		#queue_free()
+		#on screen exit
+#func _on_visibility_notifier_exit_screen():
+	#queue_free()
+
+#on impact
+func explode():
+	queue_free()
+
+
+#returns the damage this bullet will deal
+func get_damage():
+	return damage
